baa54a6aeee9129290165cdf0b61c6c6b635465a
Cristian Bîlu <bilucristian25@gmail.com>
2022-10-25 15:30:27 +0300

    Update terminal component

---
+++ src/libs/firebase.ts
@@ -0,0 +1,48 @@
+import { initializeApp, type FirebaseApp } from "firebase/app";
+import type { RemoteConfig } from "firebase/remote-config";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCm6LKcW_KIzU__h2Ur0mMZq160Gpe1Dac",
+  authDomain: "eptic-website.firebaseapp.com",
+  projectId: "eptic-website",
+  storageBucket: "eptic-website.appspot.com",
+  messagingSenderId: "325243281817",
+  appId: "1:325243281817:web:a0592703d3c37105513308",
+};
+
+let firebaseInstance: FirebaseApp | undefined = undefined;
+let remoteConfigInstance: RemoteConfig | undefined = undefined;
+
+const initialize = () => {
+  if (import.meta.env.SSR) return undefined;
+  if (firebaseInstance) return firebaseInstance;
+
+  firebaseInstance = initializeApp(firebaseConfig);
+  return firebaseInstance;
+};
+
+const getInstance = () => {
+  if (import.meta.env.SSR) return undefined;
+  if (!firebaseInstance) initialize();
+
+  return firebaseInstance;
+};
+
+const getRemoteConfig = async () => {
+  if (import.meta.env.SSR) return undefined;
+  if (remoteConfigInstance) return remoteConfigInstance;
+
+  const { getRemoteConfig, fetchAndActivate } = await import(
+    "firebase/remote-config"
+  );
+  remoteConfigInstance = getRemoteConfig(getInstance());
+  remoteConfigInstance.settings.minimumFetchIntervalMillis = 3e3;
+  await fetchAndActivate(remoteConfigInstance);
+
+  return remoteConfigInstance;
+};
+
+export default { initialize, getRemoteConfig };
