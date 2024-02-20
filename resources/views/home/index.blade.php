626a78b8f6f4cff6e2fd70e85b9f827cd4bef0c7
Cristian Bilu <cristian@eptic.ro>
2024-02-20 20:58:40 +0200

    Optimize font loading

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -7,11 +7,12 @@
     <link rel="icon" href="/favicon.png" />
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;700&display=swap" rel="stylesheet">
     <title>EPTIC Solutions</title>
     <meta name="description" content="EPTIC Solutions is a romanian based software agency." />
-    <script async src="https://analytics.eptic.ro/script.js" data-website-id="8b56bd02-1b30-4f98-93e2-7994bd258d25">
-    </script>
+    @production
+        <script async src="https://analytics.eptic.ro/script.js" data-website-id="8b56bd02-1b30-4f98-93e2-7994bd258d25">
+        </script>
+    @endproduction
     @vite(['resources/css/app.css'])
 </head>
 
